32a86373658f2d700177706f8f14a77f0f5f3120
Ulugbek <64899027+ulugbekruziboev@users.noreply.github.com>
2020-05-20 13:58:42 +0200

    Updated app.py according to RIFS
    Added RIFS selection to the main file and mirrored the change in the file name.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from DummyData import *
 from DataSetWine import *
-from ClassData import *
+from BaseData import *
 from config import *
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
+from tensorflow import keras
 from evaluation import *
 import math
 import pdb
@@ -12,24 +13,21 @@ machine_learnings_tasks = [
     "regression", "classification"
 ]
 
-wine = DataSetWine()
-data = wine.data
+#wine = DataSetWine()
+#data = wine.data
 
-
-x = wine.data[:, 0:-1]
-y = wine.data[:, -1]
-
-
-
+#x = wine.data[:, 0:-1]
+#y = wine.data[:, -1]
 
 #def __init__(self, dataset, delimiter, target, random, base_size=5, datetime=0):
-WineData = BaseData('data/winequality-red.csv', ';', 11, 1, config["nr_base_columns"])
+WineData = BaseData('data/winequality-red.csv', ';', 11, 1, config["nr_base_columns"], rifs=1)
 WineData.load()
 data = WineData.data
 
-
 x = data[:,0:-1]
 y = data[:,-1]
+
+print(x, y)
 
 #training neural network
 model = Sequential()
@@ -37,11 +35,8 @@ model = Sequential()
 model.add(Dense(10, kernel_initializer='random_normal', bias_initializer='ones', input_dim=config["max_limit_dataset_rows"] * (config["nr_base_columns"] + 1)))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
-
-model.compile(optimizer='rmsprop', learning_rate=0.000001, loss='binary_crossentropy')
+model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=1e-6), loss='binary_crossentropy')
 model.fit(x, y, epochs=1, batch_size=2, validation_split=0.2)
-
-
 
 print("--")
 
