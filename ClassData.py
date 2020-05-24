12133334d3f0625ad773df27acc5547b83f5c091
Ulugbek <64899027+ulugbekruziboev@users.noreply.github.com>
2020-05-24 23:02:52 +0200

    Bug issue resolved
    The number of columns randomly generated was not stable from time to time. 
    I hopefully found the solution. 
    It is right now appending all the permutation column-wise (so that we have a very long row in the end). 
    I did this because of our to_row method which would also be replaced with to_batch method in the nearest future.

--- ClassData.py
+++ ClassData.py
@@ -7,45 +7,58 @@ import numpy as np
 from utils import *
 from numpy.random import default_rng
 
+from config import *
+
 class BaseData():
-    def __init__(self, dataset, delimiter, target, random, base_size=5, datetime=0):
+    def __init__(self, dataset, delimiter, target, permute, base_size=5, rifs=False):
         self.dataset = dataset
         self.delimiter = delimiter
         self.target = target
-        self.random = random
+        self.permute = permute
         self.base_size = base_size
-        self.datetime = datetime
-
-    def regression_score(self, x,y):
+        self.rifs = rifs
+        
+    def regression_score(self, x, y):
         regr = linear_model.LinearRegression()
-        regr.fit(x,y)
-        return regr.score(x,y)
+        regr.fit(x, y)
+        return regr.score(x, y)
 
     def load(self):
         dataset = genfromtxt(self.dataset, delimiter=self.delimiter)[1:]
+        dataset = dataset[0:config["max_limit_dataset_rows"]]
         self.dataset = np.nan_to_num(dataset)
-        if self.datetime > 0:
-            dataset[:, self.datetime] = np.datetime64(dataset[:, self.datetime]).astype(object).day
-        self.perm = self.permutation(self.dataset, self.target, self.random)
+        self.permutation(self.dataset, self.target, self.permute)
 
-    def permutation(self, dataset, target, random):
+    def permutation(self, dataset, target, permute):
         rng = default_rng()
-        for _ in range(random):
-            random_col =  np.random.choice(np.delete(np.arange(dataset.shape[1]),target),size=self.base_size)
-            self.data = self.generate_data(random_col, self.target, self.random)
+        self.data = []
+        for _ in range(permute):
+            random_col = [i for i in np.random.choice(np.delete(np.arange(dataset.shape[1]), target), size=self.base_size, replace=False)]
+            self.data = np.append(self.data, self.generate_data(random_col, self.target, self.permute))
 
-    def generate_data(self, base_dependent_columns, independent_column, random):
+    def generate_data(self, base_dependent_columns, independent_column, permute):
+        rng = default_rng()
+
         base_x = self.dataset[:, base_dependent_columns]
+        self.base_x = base_x
         base_y = self.dataset[:, independent_column]
-        base_r2_score = self.regression_score(base_x,base_y)
+        self.base_y = base_y
+
+        if self.rifs is True:
+            random_column = rng.choice(int(np.amax(self.dataset)), size=self.dataset.shape[0], replace=True)
+            rifs_base_x = np.concatenate((base_x, random_column[:, None]), axis=1)
+            base_r2_score = self.regression_score(rifs_base_x, base_y)
+        else:
+            base_r2_score = self.regression_score(base_x, base_y)
 
         add_columns = []
         rng = default_rng()
-        #for _ in range(random):
-            #dependent_columns = [i for i in rng.choice(self.dataset.shape[1]-1, size=size, replace=False)]
-        #dependent_columns = [6, 7, 8, 9, 10]
-        dependent_columns = [i for i in rng.choice(self.dataset.shape[1], size=self.dataset.shape[1]-self.base_size, replace=False) if i not in base_dependent_columns+[independent_column]]
 
+        #dependent_columns = [6, 7, 8, 9, 10]
+        #dependent_columns = [i for i in rng.choice(self.dataset.shape[1], size=self.dataset.shape[1]-self.base_size, replace=False) if i not in base_dependent_columns+[independent_column]]
+        size = self.dataset.shape[1] - len(base_dependent_columns) - len([independent_column])
+        dependent_columns = [i for i in np.random.choice(np.delete(np.arange(self.dataset.shape[1]), np.append(independent_column, base_dependent_columns)), size=size, replace=False)]
+        #print(dependent_columns)
         for add_column in dependent_columns:
             extended_x = self.dataset[:, base_dependent_columns+[add_column]]
             score = self.regression_score(extended_x, base_y)
@@ -58,14 +71,19 @@ class BaseData():
             else:
                 _score = -(score-base_r2_score) #adding a_column does not help
 
-
-            print(_score)
-            add_columns.append([self.dataset[:,add_column],_score])
+            #print(_score)
+            add_columns.append([self.dataset[:, add_column], _score])
 
         return to_row([[base_x, add_columns]])
 
-#WineData = BaseData('data/winequality-red.csv', ';', 11, 1)
+#WineData = BaseData('data/winequality-red.csv', ';', 11, 1, rifs=True)
 #WineData.load()
+#wine = WineData.data
+#print(wine)
+#print(len(wine))
 
-#GoogleData = BaseData('data/google-safe-browsing-transparency-report-data.csv', ',', 10, 5)
+#GoogleData = BaseData('data/google-safe-browsing-transparency-report-data.csv', ',', 10, 1, rifs=True)
 #GoogleData.load()
+#data = GoogleData.data
+#print(data)
+#print(len(data))
