3a1cc57b2c26e1875885143436b1698bf8c682b8
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-06-08 20:22:54 +0200

    feat: more abstract merge_data_set method

--- app.py
+++ app.py
@@ -8,21 +8,37 @@ import pdb
 
 from evaluation import *
 from model_feedforward import *
+from functools import reduce
+
 
 
 np.set_printoptions(suppress=True)
 
 
-#def __init__(self, dataset, delimiter, target, random, base_size=5, datetime=0):
+def merge_data_sets(datasets):
+    print("inside")
+    for d in datasets:
+        d.load()
+
+
+    y_score = []
+    xy      = []
+
+    for d in datasets:
+        y_score.append(d.y_score)
+        xy.append(d.xy)
+
+    y_score = np.concatenate(y_score)
+    xy = np.concatenate(xy)
+
+
+    return xy, y_score
+
+
 WineData = BaseData('data/winequality-red.csv', ';', 11, 1, config["nr_base_columns"])
-WineData.load()
 GoogleData = BaseData('data/google-safe-browsing-transparency-report-data.csv', ',', 10, 5)
-GoogleData.load()
 
-
-xy = np.concatenate( (GoogleData.xy,WineData.xy),axis=0)
-y_score = normalize(np.concatenate( (GoogleData.y_score,WineData.y_score),axis=0))
-#y_data = normalize(np.concatenate( (GoogleData.y_data,WineData.y_data),axis=0))
+xy, y_score = merge_data_sets([WineData, GoogleData, GoogleData])
 
 '''
 from scipy.stats import pearsonr
@@ -36,36 +52,32 @@ for  i in np.arange(y_score.shape[0]):
     nfs.append(_nfs)
 '''
 
+model, i, modelhistory = best_feedforward_model(xy, y_score, True)
 
-
-
-
-model, i, modelhistory = best_feedforward_model(xy,y_score,True)
-
-
-evaluations=[]
+evaluations = []
 
 print("i:")
 print(i)
 
 print("++++")
 print("++++")
-evaluations.append(evaluation(WineData.base_xy, np.random.normal(0, 10,  WineData.base_dataset[:, 9].shape[0]), WineData.base_dataset[:,11], model))
+evaluations.append(evaluation(WineData.base_xy, np.random.normal(0, 10, WineData.base_dataset[:, 9].shape[0]),
+                              WineData.base_dataset[:, 11], model))
 print("++++")
 print("++++")
-for c in [0,1,2,3,4,5,6,7,8,9,10]:
-    evaluations.append(evaluation(WineData.base_xy, WineData.base_dataset[:, c], WineData.base_dataset[:,11], model))
+for c in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    evaluations.append(evaluation(WineData.base_xy, WineData.base_dataset[:, c], WineData.base_dataset[:, 11], model))
 print("++++")
 print("++++")
-evaluations.append(evaluation(WineData.base_xy, WineData.base_dataset[:, 11], WineData.base_dataset[:,11], model))
+evaluations.append(evaluation(WineData.base_xy, WineData.base_dataset[:, 11], WineData.base_dataset[:, 11], model))
 print("++++")
 print("++++")
 
 evaluations = np.array(evaluations)
 print(evaluations)
-plt.scatter(evaluations[:,0],evaluations[:,1])
+plt.scatter(evaluations[:, 0], evaluations[:, 1])
 plt.xlabel('NFS Score')
 plt.ylabel('Pearson')
-plt.xlim(-1,1)
-plt.ylim(-1,1)
+plt.xlim(-1, 1)
+plt.ylim(-1, 1)
 plt.show()
